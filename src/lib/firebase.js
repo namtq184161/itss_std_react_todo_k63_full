1dbb047668d6a3405f87e9e382b79b61dec975b4
Quang-NamNam <>
2022-06-27 02:30:27 +0000

    Use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCAyYJOfAmzo2YtAfLv1Ul4XMh_C2nb80",
+  authDomain: "fir-k63-qnam.firebaseapp.com",
+  projectId: "fir-k63-qnam",
+  storageBucket: "fir-k63-qnam.appspot.com",
+  messagingSenderId: "372239291091",
+  appId: "1:372239291091:web:96085879a0204678253a26",
+  measurementId: "G-38D2HXKBNN"
+};
+
+firebase.initializeApp(firebaseConfig);
